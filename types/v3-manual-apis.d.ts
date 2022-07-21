59c5b956a3b61c22e17e85a437aba5d7438a0377
longyadong <202780181@qq.com>
2022-07-21 14:25:14 +0800

    wip: port @vue/component-compiler-utils

---
+++ types/v3-manual-apis.d.ts
@@ -0,0 +1,8 @@
+import { SetupContext } from './v3-setup-context'
+import { CreateElement, Vue } from './vue'
+
+export function getCurrentInstance(): { proxy: Vue } | null
+
+export const h: CreateElement
+
+export function useSlots(): SetupContext['slots']
